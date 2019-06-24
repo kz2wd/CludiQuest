931f4eaa48ecf7bdaf72d93309615db7e72a59c5
Kamiren <xkamiren@gmail.com>
2019-06-24 22:54:21 +0200

    Added hero file

---
+++ hero.py
@@ -0,0 +1,9 @@
+class hero:
+    def __init__(self, Name, Attack, Def, Health):
+        self.Name = Name
+        self.Attack = Attack
+        self.Def = Def
+        self.Health = Health
+
+Knight = hero("Knight", 10, 10, 100)
+

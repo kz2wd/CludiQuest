90da13a105ca212647b41e950f46c94bb3835323
Kamiren Cache <xkamiren@gmail.com>
2019-06-24 22:16:05 +0200

    added main files.

--- Begin.py
+++ Begin.py
@@ -16,8 +16,8 @@ class MyClient(discord.Client):
         if message.content.startswith('$play'):
             await message.channel.send('```Mhhhh.... Okay thats```')
 
-class discord.Message:
+class discord(Message)
 
 
 client = MyClient()
-client.run('NTkyNzgwOTMyODM5MjQzNzgy.XREUaw.uX3yVNoSNYFmBPsREVqJ1MupWag')
+client.run('')

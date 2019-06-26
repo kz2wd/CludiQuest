da4a20f804d5fdbfbdd431f75c3b3a68666c0381
kz2wd <itrotky@gmail.com>
2019-06-26 19:38:35 +0200

    Working player list with id.

--- Begin.py
+++ Begin.py
@@ -27,9 +27,34 @@ class MyClient(discord.Client):
         if message.author == self.user:
             if message.content == msg.start:
                 await message.add_reaction(emote.start)
+                global chan
+                chan = message.channel
         elif message.content == '!!start':
             await message.channel.send(msg.start)
 
+    async def on_reaction_add(self, reaction, user):
+        global x
+        global chan
+        if chan != 0:
+            if x == 0:
+                x += 1
+            elif 0 < x < 5:
+                player_list[x - 1] = user.id
+                x += 1
+                print(player_list)
+                print(x - 1)
+                if x == 2:  # put 4 instead of 2, but there is just me and the bot :'(
+                    print("game starting")
+
+                    game = party.Party(chan, player_list)
+                    await chan.send(msg.kit_choose)
+
+
+x = 0
+chan = 0
+player_list = [0, 0, 0, 0]
 
 client = MyClient()
+
+
 client.run(Variable.x)
